568053862961764afa5a60fe86f1d8d048f391b0
Raghav-ust <124768883+Raghav-ust@users.noreply.github.com>
2023-03-10 08:24:23 +0530

    editting post

--- src/Component/Posts.jsx
+++ src/Component/Posts.jsx
@@ -1,22 +1,50 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
-import PostList from "./PostList";
-import ResponsiveDrawer from './ResponsiveDrawer';
-const baseURL = " https://jsonplaceholder.typicode.com/posts";
+import { Box, Typography, Accordion, AccordionSummary, AccordionDetails } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
+// import axios from "axios";
+// import PostList from "./PostList";
+// import ResponsiveDrawer from './ResponsiveDrawer';
+// const baseURL = " https://jsonplaceholder.typicode.com/posts";
 function Posts() {
-    const [postList, setPostList] = useState([]);
-    useEffect(() => {
-        axios.get(baseURL).then((response) => {
-            setPostList(response.data);
-        });
-    }, [])
-    return (
-        <div className="contentWrap">
-            <ResponsiveDrawer />
-            <div className="listItems">
-                <PostList list={postList} />
-            </div>
-        </div>
-    );
+    const [posts, setPosts] = useState([]);
+  const [comments, setComments] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(response => response.json())
+      .then(data => setPosts(data));
+
+    fetch('https://jsonplaceholder.typicode.com/comments')
+      .then(response => response.json())
+      .then(data => setComments(data));
+  }, []);
+
+  const getCommentsByPostId = (postId) => {
+    return comments.filter(comment => comment.postId === postId);
+  }
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      {posts.map(post => (
+        <Accordion key={post.id}>
+          <AccordionSummary expandIcon={<ExpandMoreIcon />} aria-controls={`post-${post.id}-content`} id={`post-${post.id}-header`}>
+            <Typography variant="h5">{post.title}</Typography>
+          </AccordionSummary>
+          <AccordionDetails>
+            <Typography variant="body1">{post.body}</Typography>
+          </AccordionDetails>
+          <AccordionDetails>
+            {getCommentsByPostId(post.id).map(comment => (
+              <Box key={comment.id} sx={{ display: 'flex', flexDirection: 'column', marginTop: '8px' }}>
+                <Typography variant="subtitle1">{comment.name}</Typography>
+                <Typography variant="body2">{comment.body}</Typography>
+              </Box>
+            ))}
+          </AccordionDetails>
+        </Accordion>
+      ))}
+    </Box>
+  );
 }
 export default Posts;
