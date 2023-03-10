4c3da5a93e27f11182bd6416aaa04703fe21f933
Raghav-ust <124768883+Raghav-ust@users.noreply.github.com>
2023-03-10 08:10:50 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import { Routes, Route, useLocation } from 'react-router-dom';
+import './App.css';
+import Dashboard from './Component/Dashboard';
+import Login from './Login';
+import RegistrationPage from './RegistrationPage';
+import ProfilePage from './ProfilePage';
+
+import ResponsiveDrawer from './Component/ResponsiveDrawer';
+import Products from './Component/Products';
+import Posts from './Component/Posts';
+import Todos from './Component/Todos';
+import UsersListing from './Component/UsersListing';
+
+function App() {
+  
+  const location = useLocation();
+
+  return (
+    <div className="App">
+
+      { location.pathname === '/' || 
+      location.pathname === '/profilePage' || 
+      location.pathname === '/login' ?
+        <Routes>
+          <Route path="/" element={<RegistrationPage />} />
+          <Route path="/profilepage" element={<ProfilePage />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+        :
+        <>
+          <ResponsiveDrawer />
+          <Routes>
+            <Route path="/dashboard" element={<Dashboard />} />
+            <Route path="/products" element={<Products />} />
+            <Route path="/posts" element={<Posts />} />
+            <Route path="/userListing" element={<UsersListing />} />
+            <Route path="/todo" element={<Todos />} />
+
+          </Routes>
+        </>
+  }
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/Dashboard.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Bar } from 'react-chartjs-2';
+import LineChart from './LineChart';
+import { HorizontalBarChart } from './HorizontalBarChart';
+import { GroupedBar } from './GroupedBar';
+import ResponsiveDrawer from './ResponsiveDrawer';
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+export const barChartOptions = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Chart.js Bar Chart',
+    },
+  },
+};
+
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const barChartData = {
+  labels,
+  datasets: [
+    {
+      label: 'Dataset 1',
+      data: [50, 30],
+      backgroundColor: 'rgba(255, 99, 132, 0.5)',
+    },
+    {
+      label: 'Dataset 2',
+      data: [20, 10],
+      backgroundColor: 'rgba(53, 162, 235, 0.5)',
+    },
+  ],
+};
+
+
+
+
+export default function Dashboard() {
+  return  <>
+  <div className='chartlist'>
+    <div className='chart'>
+      <Bar options={barChartOptions} data={barChartData} />
+      <LineChart />
+    </div>
+    <div className='chart'>
+      <HorizontalBarChart /> 
+      <GroupedBar />
+    </div>
+  </div>
+  </>
+}
+
+
